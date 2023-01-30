76646328c853b85e823338af51adffa235596eb4
Roman Chuchval <rchuchval@gmail.com>
2023-01-30 23:13:19 +0300

    adding logic and functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Counter} from "./Counter";
 import {SuperButton} from "./SuperButton";
+import {Settings} from "./Settings";
 
 
 function App() {
+    const [startNum, setStartNum] = useState<number>(0)
+    const [maxNum, setMaxNum] = useState<number>(5)
+    const [count, setCount] = useState<number>(startNum)
+    const [error, setError] = useState<string>('')
+    const [message, setMessage] = useState<string>('')
 
-    const [count, setCount] = useState<number>(0)
     const increment = () => {
-        setCount(count + 1)
+        count < maxNum && setCount(count + 1)
+    }
+    const reset = () => {
+        setCount(startNum)
     }
 
-    const reset = () => {
-        setCount(0)
+    const displayErrorMessage = () => {
+        if (maxNum <= startNum || startNum < 0) {
+            setError('Incorrect value!')
+        } else if (maxNum > startNum) {
+            setError('')
+        }
     }
+
+    const displaySettingMessage = (isDisplay: boolean) => {
+        if (isDisplay) {
+            setMessage(`Enter values and press 'set'`)
+        } else {
+            setMessage('')
+            setCount(startNum)
+        }
+    }
+    useEffect(() => {
+        displayErrorMessage()
+    }, [maxNum, startNum])
+
 
     return (
         <div className="App">
-            <Counter count={count}/>
-            <div>
-                <SuperButton disabled={count === 5} name={'Increment'} callback={increment}/>
-                <SuperButton disabled={count === 0} name={'Reset'} callback={reset}/>
+            <div className={"Counter_wrapper"}>
+                <Counter error={error}
+                         count={count}
+                         startNum={startNum}
+                         maxNum={maxNum}
+                         displayMessage={message}>
+                    {error ? error : message ? message : count}
+                </Counter>
+
+                <div className={"Counter_btn_wrapper"}>
+                    <SuperButton disabled={count === maxNum || !!message}
+                                 name={'Increment'}
+                                 callback={increment}/>
+
+                    <SuperButton disabled={count === startNum || !!message}
+                                 name={'Reset'}
+                                 callback={reset}/>
+                </div>
+            </div>
+            <div className={'Settings_wrapper'}>
+                <Settings
+                    message={message}
+                    displaySettingMessage={displaySettingMessage}
+                    error={error}
+                    startNum={startNum}
+                    setStartNum={setStartNum}
+                    maxNum={maxNum}
+                    setMaxNum={setMaxNum}
+                />
             </div>
         </div>
     );
